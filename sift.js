6a7cdda7c67bff1ea92f1aff7078ec97f1da7023
andrewjones <aljones15@gmail.com>
2018-04-11 17:11:05 +0800

    equals works again

--- sift.js
+++ sift.js
@@ -3,9 +3,19 @@ var sift = require('sift');
 //use a custom compare function so we can search on ObjectIDs
 var compare = sift.compare;
 sift.compare = function(a, b) {
-  if(a && b && a._bsontype && b._bsontype) {
-    return a.equals(b)? 0 : (compare(time(a), time(b)) || compare(a.str, b.str));
-  }
+        if (a.equals) {
+            return a.equals(b)
+                ? 0
+                : compare(time(a), time(b)) || compare(a.str, b.str);
+        }
+        // the mongoose ObjectId lacks the same interface as the mock so we improvise here
+        // @param {BsonObject} a - this in the in memory ObjectID with out .equals
+        // @param {String|BsonObject} b - this is the hex ObjectID String sent to the route
+        if (a.id) {
+            const ahex = a.id.toString('hex').trim();
+            const bhex = b.id ? b.id.toString('hex').trim() : b;
+            return ahex == bhex ? 0 : 1;
+        }
   return compare(a,b);
 };
 function time(id) {
