de158468b6b116c73b04a2a81a35997cc96cf79e
andrewjones <aljones15@gmail.com>
2018-04-16 14:50:53 +0800

    forgot null check

--- sift.js
+++ sift.js
@@ -3,7 +3,7 @@ var sift = require('sift');
 //use a custom compare function so we can search on ObjectIDs
 var compare = sift.compare;
 sift.compare = function(a, b) {
-        if (a.equals) {
+        if (a && a.equals) {
             return a.equals(b)
                 ? 0
                 : compare(time(a), time(b)) || compare(a.str, b.str);
@@ -11,7 +11,7 @@ sift.compare = function(a, b) {
         // the mongoose ObjectId lacks the same interface as the mock so we improvise here
         // @param {BsonObject} a - this in the in memory ObjectID with out .equals
         // @param {String|BsonObject} b - this is the hex ObjectID String sent to the route
-        if (a.id) {
+        if (a && a.id) {
             const ahex = a.id.toString('hex').trim();
             const bhex = b.id ? b.id.toString('hex').trim() : b;
             return ahex == bhex ? 0 : 1;
